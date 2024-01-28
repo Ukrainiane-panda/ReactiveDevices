2d0ccaa89c8857625b421b80fdd86cfb8429ae97
Ukrainian_panda <94929618+Ukrainiane-panda@users.noreply.github.com>
2024-01-28 22:33:59 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,61 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import { FavouritesProvider } from './context/FavouritesProvider';
+import { SearchProvider } from './context/SearchContext';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { Product } from './types/Product';
+import { getProducts } from './helpers/fetchClient';
+import { CartProvider } from './context/CartProvider';
+
 import './App.scss';
 
-interface Props {
-  onClick: () => void;
-}
+const App = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-export const Provider: React.FC<Props> = React.memo(
-  ({ onClick, children }) => (
-    <button
-      type="button"
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  ),
-);
+  /* eslint-disable no-console */
+  console.log(products);
 
-export const App: React.FC = () => {
+  const fetchProducts = async () => {
+    setIsLoading(!isLoading);
+
+    try {
+      const productsFromServer = await getProducts() as Product[];
+
+      localStorage.setItem('products', JSON.stringify(productsFromServer));
+
+      setProducts(productsFromServer);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const storedProducts = localStorage.getItem('products');
+
+    if (storedProducts) {
+      setProducts(JSON.parse(storedProducts));
+    } else {
+      fetchProducts();
+    }
+  }, []);
+
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>
-        <TodoList />
-      </Provider>
-    </div>
+    <CartProvider>
+      <FavouritesProvider>
+        <SearchProvider>
+          <>
+            <Header />
+            <main>
+              <Outlet />
+            </main>
+            <Footer />
+          </>
+        </SearchProvider>
+      </FavouritesProvider>
+    </CartProvider>
   );
 };
+
+export default App;
